d60feb6a181e6a93deb2077811751ba11481b797
Giovanna Nogueira <giovannarnogueira@gmail.com>
2025-01-18 22:10:23 -0300

    Rsolução dos exercícios 1 e 2 de variáveis e constantes.

---
+++ Curso JS da Trybe/Variaveis e Constantes/exercicio1.js
@@ -0,0 +1,7 @@
+/***** Resolução do Exercício 1 *****/
+
+let cor = 'verde';
+
+/***** Enviando resultado para o arquivo html *****/
+
+document.getElementById("resultado").innerHTML = "A variável cor tem como valor " + cor;
---
+++ Curso JS da Trybe/Variaveis e Constantes/exercicio2.js
@@ -0,0 +1,9 @@
+/***** Resolução do Exercício 2 *****/
+
+let cidade = 'Parauapebas';
+
+cidade = 'Rio de Janeiro';
+
+/***** Enviando resultado para o arquivo html *****/
+
+document.getElementById("resultado").innerHTML = "A variável cidade mudou para " + cidade;
