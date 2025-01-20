98a5ac48c25de19cc6539fbdddac87b23685332a
Giovanna Nogueira <giovannarnogueira@gmail.com>
2025-01-20 19:53:07 -0300

    Rsolução dos exercícios 3 e 4 de variáveis e constantes.

---
+++ Curso JS da Trybe/Variaveis e Constantes/exercicio3.js
@@ -0,0 +1,7 @@
+/***** Resolução do Exercício 3 *****/
+
+const meuNome = 'Giovanna Nogueira';
+
+/***** Enviando resultado para o arquivo html *****/
+
+document.getElementById("resultado").innerHTML = `A variável meuNome é uma const de valor "<strong>${meuNome}</strong>"`;
---
+++ Curso JS da Trybe/Variaveis e Constantes/exercicio4.js
@@ -0,0 +1,7 @@
+/***** Resolução do Exercício 1 *****/
+
+let minhaIdade = 26;
+
+/***** Enviando resultado para o arquivo html *****/
+
+document.getElementById("resultado").innerHTML = `minhaIdade é do tipo number e de valor <strong>${minhaIdade}</strong>`;
