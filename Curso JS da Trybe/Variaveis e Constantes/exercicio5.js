b80297179105d25ab4feaa67dde2b883f4696a6b
Giovanna Nogueira <giovannarnogueira@gmail.com>
2025-01-21 20:28:34 -0300

    Rsolução do exercício 5 de variáveis e constantes.

---
+++ Curso JS da Trybe/Variaveis e Constantes/exercicio5.js
@@ -0,0 +1,8 @@
+/***** Resolução do Exercício 5 *****/
+
+let email = 'email@trybe.com';
+email = 'outroEmail@trybe.com';
+
+/***** Enviando resultado para o arquivo html *****/
+
+document.getElementById("resultado").innerHTML = "Foi possível atribuir um novo valor à variável email: " + email;
